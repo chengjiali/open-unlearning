2191f84f1502cb7b51c50a3b9e36f7db9be77663
Jiali Cheng <20452970+chengjiali@users.noreply.github.com>
2026-01-08 15:45:46 -0500

    add path to save difficulty

--- src/compute_sample_difficulty.py
+++ src/compute_sample_difficulty.py
@@ -25,7 +25,7 @@ def main(cfg: DictConfig):
     assert model_cfg is not None, "Invalid model yaml passed in train config."
     model, tokenizer = get_model(model_cfg)
 
-    task_name = cfg.get('task_name').lower()
+    task_name = cfg.get('task_name')
 
     if 'tofu' in task_name:
         dataset_name = 'tofu'
@@ -41,11 +41,13 @@ def main(cfg: DictConfig):
 
     # eval_cfgs = cfg.eval
     # for evaluator_name, eval_cfg in eval_cfgs.items():
+    import os
+    os.makedirs(f"saves/sample_difficulty/{dataset_name}", exist_ok=True)
     eval_args = {
         "template_args": template_args,
         "model": model,
         "tokenizer": tokenizer,
-        "output_dir": 'saves/sample_difficulty'
+        "output_dir": f"saves/sample_difficulty/{dataset_name}/{task_name}"
     }
     evaluator = EvaluatorComputeSampleDifficulty(dataset_name, data_split, cfg, **eval_args)
     _ = evaluator.compute_sample_difficulty(**eval_args)
